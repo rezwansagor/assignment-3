88e60bd2fddc99b7104082536f2e946ac7b85b1f
Rezwan Sagor <rezwansagor100@gmail.com>
2025-02-01 22:51:35 +0600

    some fixes in news section and price section for mobile device

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      animation: {
+        wiggle: "wiggle 1s ease-in-out infinite",
+      },
+      keyframes: {
+        wiggle: {
+          "0%": {
+            transform: "rotate(-90deg)",
+          },
+          "50%": {
+            transform: "rotate(90deg)",
+          },
+          "100%": {
+            transform: "rotate(-90deg)",
+          },
+        },
+      },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+};
